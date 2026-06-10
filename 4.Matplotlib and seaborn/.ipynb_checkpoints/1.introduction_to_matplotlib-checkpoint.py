9b8dadb5dcc6d1fa4f64271d97195c001d88adc1
Abhishek-x23 <aseth6779@gmail.com>
2026-06-10 14:11:36 +0530

    matplotlib done

---
+++ 4.Matplotlib and seaborn/.ipynb_checkpoints/1.introduction_to_matplotlib-checkpoint.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+plt.plot([2,3,5],[5,6,9])
+plt.show()
+plt.plot([2,3,9],[2,4,8])
+plt.show()
---
+++ 4.Matplotlib and seaborn/1.introduction_to_matplotlib.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.plot([2,3,5],[5,6,9])
+plt.show()
+plt.plot([2,3,9],[2,4,8])
+plt.show()
+
+# run this pyhton file in terminal to grt thr graphs pop out
